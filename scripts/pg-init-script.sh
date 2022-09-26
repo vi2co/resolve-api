9270b987577a617cb9088fcf8548c7dcd9a10e7f
Vitaliy Shynkar <vshynkar@gmail.com>
2022-09-26 14:33:51 -0700

    DOCKER-COMPOSE: move db init script into the Helm chart dir

--- scripts/pg-init-script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-  CREATE TABLE lookup (id serial primary key, addresses VARCHAR(255), client_ip VARCHAR(32), created_at VARCHAR(32), domain_name VARCHAR (50));
-EOSQL
